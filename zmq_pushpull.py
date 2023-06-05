c01bcc493dd6632c9f418eba1df3901738a7cd24
lolotrgeek <jon@simplyenable.com>
2023-06-05 11:24:30 -0500

    feat: actual push/pull with basic routing

--- zmq_pushpull.py
+++ zmq_pushpull.py
@@ -9,14 +9,15 @@ class Pusher():
     def __init__(self):
         self.highwatermark = 3 # how many messages to keep in , reduce the throughput but decreases a pull returning None
         self.lowwatermark = 1
-
+        self.context = zmq.Context()
+        self.zmq_socket = self.context.socket(zmq.PUSH)
+        self.zmq_socket.connect("tcp://127.0.0.1:5555")
+        
 
     def push(self, message):
         try:
-            context = zmq.Context()
-            zmq_socket = context.socket(zmq.PUSH)
-            zmq_socket.bind("tcp://127.0.0.1:5557")
-            zmq_socket.send_json(message)
+            self.zmq_socket.send_json(message)
+            return True
         except Exception as e:
             print(e)
             return None
@@ -24,18 +25,36 @@ class Pusher():
 
 class Puller():
     def __init__(self):
-        pass
+        self.context = zmq.Context()
+        self.results_receiver = self.context.socket(zmq.PULL)
+        self.results_receiver.connect("tcp://127.0.0.1:5556")
 
     def pull(self):
         try:
-            context = zmq.Context()
-            results_receiver = context.socket(zmq.PULL)
-            results_receiver.connect("tcp://127.0.0.1:5557")
-            return results_receiver.recv_json()
+            msg = self.results_receiver.recv_json()
+            return msg
         except Exception as e:
             print(e)
             return None
 
+
+class Router():
+    def __init__(self):
+        self.context = zmq.Context()
+        self.producer_socket = self.context.socket(zmq.PULL)
+        self.producer_socket.bind("tcp://127.0.0.1:5555")
+        self.consumer_socket = self.context.socket(zmq.PUSH)
+        self.consumer_socket.bind("tcp://127.0.0.1:5556")
+
+    def route(self):
+        try:
+            while True:
+                msg = self.producer_socket.recv_json()
+                self.consumer_socket.send_json(msg)
+
+        except Exception as e:
+            print(e)
+            return    
 
 def push():
     try:
@@ -46,7 +65,7 @@ def push():
         max_count = 10
         while True:
             if(time()-start >= max_count):
-                end = time()
+                print(messages / max_count, "messages pushed per second and ", nones, "nones")
                 break
             msg = p.push({"Hello": "World"})
             if msg == None:
@@ -56,8 +75,6 @@ def push():
     
     except KeyboardInterrupt:
         return
-    finally:
-        print(messages / max_count, "messages sent per second and ", nones, "nones")
 
 def pull():
     try:
@@ -66,46 +83,52 @@ def pull():
         nones = 0
         messages = 0
         start = time()
-        max_count = 10
+        max_count = 11
         while True:
             if(time()-start >= max_count):
-                end = time()
+                print(messages / max_count, "messages pulled per second and ", nones, "nones")
                 break
             msg = s.pull()
             if msg == None:
                 nones += 1
             elif msg:
                 messages += 1
-
-    except KeyboardInterrupt:
-        return messages
     
-    finally:
-        print(messages / max_count, "messages received per second and ", nones, "nones")
+    except Exception as e:
+        print(e)
+        return None
+
+def route():
+    try:
+        r = Router()
+        r.route()
+    except Exception as e:
+        print(e)
+        return None    
+
 
 if __name__ == '__main__':
-
-    messages = 0
-
-    pusher = Process(target=push)
-    puller = Process(target=pull)
-
-    pusher.start()
-    puller.start()
-
-    count = 0
-    max_count = 1
-
     try:
-        start = time()
-        end = None
+
+        pusher = Process(target=push)
+        puller = Process(target=pull)
+        router = Process(target=route)
+
+        pusher.start()
+        puller.start()
+        router.start()
+
         while True:
             sleep(.1)
 
     except KeyboardInterrupt:
         print("attempting to close processes..." )
+        pusher.terminate()
+        puller.terminate()
+        router.terminate()
         pusher.join()
         puller.join()
+        router.join()
         print("processes successfully closed")
 
     finally:
