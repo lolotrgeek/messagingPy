ffbd661d65f2826033dcbf0d99a4a4fcd8b3d3ba
lolotrgeek <jon@simplyenable.com>
2023-06-08 15:25:02 -0500

    feat: topic based reqres

--- zmq_reqres.py
+++ zmq_reqres.py
@@ -1,52 +1,46 @@
-from multiprocessing import Process, Pipe
+from multiprocessing import Process
 from time import time, sleep
 import zmq
-
+import traceback
 
 class Requester():
     def __init__(self):
-        pass
+            self.context = zmq.Context()
+            self.socket = self.context.socket(zmq.REQ)
+            self.socket.connect('tcp://127.0.0.1:5555')
 
-    def request(self, message):
+    def request(self, topic, args=None):
         try:
-            context = zmq.Context()
-            socket = context.socket(zmq.REQ)
-            socket.connect('tcp://127.0.0.1:5555')
-            socket.send(message)
-            return socket.recv()
+            self.socket.send_json({'topic': topic, 'args': args})
+            return self.socket.recv_json()
         except Exception as e:
             print(e)
             return None
 
 class Responder():
-    def __init__(self):
-        pass
+    def __init__(self, topics={}):
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.REP)
+        self.socket.bind('tcp://127.0.0.1:5555')
+        self.topics = topics
 
-    def respond(self, topic, response):
+    def respond(self):
         try:
-            context = zmq.Context()
-            socket = context.socket(zmq.REP)
-            socket.bind('tcp://127.0.0.1:5555')
-            nones = 0
-            messages = 0
-            while True:
-                msg = socket.recv()
-                if msg == topic:
-                    messages += 1
-                    socket.send(response)
-                else:
-                    nones += 1
-                    socket.send(None)
-                
+            msg = self.socket.recv_json()
+            if msg["topic"] in self.topics.keys():
+                response = self.topics[msg['topic']](msg['args'])
+                self.socket.send_json(response)
+                return response
+            else:
+                self.socket.send_json(None)
+                return None
         except Exception as e:
-            print(e)
+            print("Response Error", e)
+            traceback.print_exc()
             return None
-        finally:
-            print(messages, "messages responded per second and ", nones, "nones")
 
-def request():
+def request(topic, message):
     try:
-        sleep(.5)
         r = Requester()
         nones = 0
         messages = 0
@@ -54,25 +48,42 @@ def request():
         max_count = 10
         while True:
             if(time()-start >= max_count):
-                end = time()
+                print(messages / max_count, "messages received per second and ", nones, "nones")
                 break
-            msg = r.request(b"hello")
+            msg = r.request(topic, message)
             if msg == None:
                 nones += 1
-            elif msg:
-                messages += 1        
+            elif msg == "world":
+                messages += 1
+            elif msg == "cruel_world":
+                messages += 1
     except Exception as e:
         print(e)
         return None
-    finally:
-        print(messages / max_count, "messages received per second and ", nones, "nones")
-    
 
 def respond():
     try:
-        s = Responder()
-        s.respond(b"hello", b"world")
-
+        topics = {
+            "hello": lambda x: "world" if x == "world" else None,
+            "goodbye": lambda x: "cruel_world" if x == "cruel_world" else None,
+            }
+        s = Responder(topics)
+        nones = 0
+        messages = 0
+        start = time()
+        max_count = 11        
+        while True:
+            if(time()-start >= max_count):
+                print(messages / max_count, "messages responded per second and ", nones, "nones")
+                break
+            response = s.respond()
+            if response == None:
+                nones += 1
+            elif response == "world":
+                messages += 1
+            elif response == "cruel_world":
+                messages += 1    
+                    
     except Exception as e:
         print(e)
         return None
@@ -80,31 +91,25 @@ def respond():
 
 if __name__ == '__main__':
 
-    messages = 0
-
     responder = Process(target=respond)
-    requester = Process(target=request)
+    requester = Process(target=request, args=("hello", "world"))
+    requester_bye = Process(target=request, args=("goodbye", "cruel_world"))
 
     responder.start()
     requester.start()
-
-    count = 0
-    max_count = 12
-
+    requester_bye.start()
     try:
         while True:
-            start = time()
-            while True:
-                if(time()-start >= max_count):
-                    end = time()
-                    break
-            responder.terminate()
-            requester.terminate()
+            sleep(.1)
 
     except KeyboardInterrupt:
         print("attempting to close processes..." )
+        responder.terminate()
+        requester.terminate()
+        requester_bye.terminate()
         responder.join()
         requester.join()
+        requester_bye.join()
         print("processes successfully closed")
 
     finally:
